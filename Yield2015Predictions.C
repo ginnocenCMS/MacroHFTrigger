c3cdf28f831aabbfeb99bb555ce2b4f41be9cce3
gian michele innocenti <ginnocen@cern.ch>
2015-09-11 14:30:07 +0200

    upd

--- Yield2015Predictions.C
+++ Yield2015Predictions.C
@@ -6,13 +6,36 @@ void Yield2015Predictions(){
   Double_t datapoints[da_BIN_NUM] = {3.,4.,5.,6.25,8.,10.,12.,14.5,18.,24.,34.};
   Double_t ptcenters[da_BIN_NUM] = {2.91.,3.91.,4.91.,6.10.25,7.78,9.79,11.8,14.17,17.52.,22.71,31.89};
 
-  #define NptbinTrigger 15
-  Double_t ptbinTrigger[NptbinTrigger+1] = {5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80};
-  Double_t prescales[NptbinTrigger] = {1,300,300,300,300,200,200,200,200,1,1,1,1,1,1};
+  #define NptbinTrigger 7
+  Double_t ptbinTrigger[NptbinTrigger+1] = {5,10,15,20,25,30,35,40};
+  
+  #define NptbinTriggerext 19
+  Double_t ptbinTriggerext[NptbinTriggerext+1] = {5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100};
+  Double_t prescales[NptbinTriggerext] = {300,300,300,300,300,300,300,300,300,300,300,300,300,300,300,300,300,300,300};
+  
+  /*
+  index=6, pt centre=37.5,cross section=841.357,ratio over 28-40=1
+  index=7, pt centre=42.5,cross section=427.663,ratio over 28-40=0.508301
+  index=8, pt centre=47.5,cross section=217.087,ratio over 28-40=0.25802
+  index=9, pt centre=52.5,cross section=116.917,ratio over 28-40=0.138962
+  index=10, pt centre=57.5,cross section=66.0846,ratio over 28-40=0.0785453
+  index=11, pt centre=62.5,cross section=37.4163,ratio over 28-40=0.0444713
+  index=12, pt centre=67.5,cross section=24.3227,ratio over 28-40=0.028909
+  index=13, pt centre=72.5,cross section=15.2512,ratio over 28-40=0.018127
+  index=14, pt centre=77.5,cross section=9.79585,ratio over 28-40=0.0116429
+  index=15, pt centre=82.5,cross section=6.426,ratio over 28-40=0.00763767
+  index=16, pt centre=87.5,cross section=4.12146,ratio over 28-40=0.00489858
+  index=17, pt centre=92.5,cross section=2.98778,ratio over 28-40=0.00355114
+  index=18, pt centre=97.5,cross section=2.07216,ratio over 28-40=0.00246288
+  */
+   int binreference=6;  
+  const int rescaledtobin=12;  
+  double ratiorescale[rescaledtobin]={0.508301,0.25802,0.138962,0.0785453,0.0444713,0.028909,0.018127,0.0116429,0.00763767,0.00489858,0.00355114,0.00246288};
+
   //#define NptbinTrigger 11
   //Double_t ptbinTrigger[NptbinTrigger+1] = {2.5,3.5,4.5,5.5,7.,9.,11.,13.,16.,20.,28.,40.};
   
-  TH1F* hfitref = new TH1F("hfitref","",NptbinTrigger,ptbinTrigger);
+  TH1F* hfitref = new TH1F("hfitref","",NptbinTriggerext,ptbinTriggerext);
   
   TFile*file=new TFile("DataCMSfile/PromptRAA_D0_PbPb_spectrum_fonll_effunpre_cent0to100_ptbin12_y1_dataplusfonll.root");
   TH1F* hSpectrumOriginal=(TH1F*)file->Get("D0_pbpb_spectrum");
@@ -81,18 +104,26 @@ void Yield2015Predictions(){
   crescaled->SaveAs("crescaled.pdf");
   
   TH1F* hrebinnedspectrum = new TH1F("hrebinnedspectrum","",NptbinTrigger,ptbinTrigger);
-  TH1F* hpredictionRawYields = new TH1F("hpredictionRawYields","",NptbinTrigger,ptbinTrigger);
-  
+  TH1F* hrebinnedspectrumext = new TH1F("hrebinnedspectrumext","",NptbinTriggerext,ptbinTriggerext);
+  TH1F* hpredictionRawYields = new TH1F("hpredictionRawYields","",NptbinTriggerext,ptbinTriggerext);
+  double integ=0;
   for (int j=0;j<NptbinTrigger;j++){
-    double integ = fdata->Integral(ptbinTrigger[j],ptbinTrigger[j+1])/hfitref->GetBinWidth(j+1)/prescales[j];
-    //double integ = hSpectrum->GetBinContent(hSpectrum->FindBin(datapoints[j]));
-    //double yieldsfinal=integ*hfitref->GetBinWidth(j+1)*efficiency[j];
+    integ = fdata->Integral(ptbinTrigger[j],ptbinTrigger[j+1])/hfitref->GetBinWidth(j+1);
     hrebinnedspectrum->SetBinContent(j+1,integ);
-    hrebinnedspectrum->SetBinError(j+1,TMath::Sqrt(integ));
-    hpredictionRawYields->SetBinContent(j+1,integ*energyfactor*hfitref->GetBinWidth(j+1));
-    hpredictionRawYields->SetBinError(j+1,TMath::Sqrt(integ*energyfactor*hfitref->GetBinWidth(j+1)));
+  }
+  
+  for (int j=0;j<NptbinTriggerext;j++){
+    integ=hrebinnedspectrum->GetBinContent(j+1);
+    hrebinnedspectrumext->SetBinContent(j+1,integ);
+    if(j>6){
+      integ=hrebinnedspectrum->GetBinContent(binreference+1)*ratiorescale[j-6-1];
+      hrebinnedspectrumext->SetBinContent(j+1,integ);
+      hrebinnedspectrumext->SetBinError(j+1,integ);
+    }
+    hpredictionRawYields->SetBinContent(j+1,integ*energyfactor*hfitref->GetBinWidth(j+1)/prescales[j]);
     cout<<"Yield expected="<<integ*energyfactor*hfitref->GetBinWidth(j+1)<<endl;
   }
+
   
   //TH1F* hratio=(TH1F*)hrebinnedspectrum->Clone("hratio");
   //hratio->Divide(hDen);
@@ -103,7 +134,7 @@ void Yield2015Predictions(){
 
   TCanvas* cprediction =  new TCanvas("cprediction","",600,400);
   cprediction->SetLogy();
-  TH2F* hempty_cprediction=new TH2F("hempty_cprediction","",10,0,80.,10.,1,1000000);
+  TH2F* hempty_cprediction=new TH2F("hempty_cprediction","",10,0,100.,10.,0.001,1000000000);
   hempty_cprediction->SetStats(0);
   hempty_cprediction->GetXaxis()->SetTitle("p_{T} (GeV/c)");
   hempty_cprediction->GetYaxis()->SetTitle("Yields from Fit @ 5.02 TeV, eff x acc=1");
