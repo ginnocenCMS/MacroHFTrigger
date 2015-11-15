c94f540892d6c7f5a8e6f35155cafd4655883b73
ginnocen <ginnocen@cern.ch>
2015-11-16 00:58:38 +0100

    update seeding

--- plotMBRatesPP.C
+++ plotMBRatesPP.C
@@ -30,6 +30,7 @@ void plotMBRatesPP(int totall1rate=1600*1.4*11245){
    int HLT_AK4PFDJet60_Eta2p1_v1;
    int HLT_AK4PFDJet80_Eta2p1_v1;
    int L1_SingleJet16_BptxAND;
+   int L1_SingleJet24_BptxAND;
    int L1_SingleJet28_BptxAND;
    int L1_SingleJet40_BptxAND;
    int L1_SingleJet48_BptxAND;
@@ -50,6 +51,7 @@ void plotMBRatesPP(int totall1rate=1600*1.4*11245){
    hlt->SetBranchAddress("HLT_AK4PFDJet60_Eta2p1_v1",&HLT_AK4PFDJet60_Eta2p1_v1);
    hlt->SetBranchAddress("HLT_AK4PFDJet80_Eta2p1_v1",&HLT_AK4PFDJet80_Eta2p1_v1);
    hlt->SetBranchAddress("L1_SingleJet16_BptxAND",&L1_SingleJet16_BptxAND);
+   hlt->SetBranchAddress("L1_SingleJet24_BptxAND",&L1_SingleJet24_BptxAND);
    hlt->SetBranchAddress("L1_SingleJet28_BptxAND",&L1_SingleJet28_BptxAND);
    hlt->SetBranchAddress("L1_SingleJet40_BptxAND",&L1_SingleJet40_BptxAND);
    hlt->SetBranchAddress("L1_SingleJet48_BptxAND",&L1_SingleJet48_BptxAND);
@@ -70,6 +72,7 @@ void plotMBRatesPP(int totall1rate=1600*1.4*11245){
    double counter_HLT_AK4PFDJet60_Eta2p1_v1=0;
    double counter_HLT_AK4PFDJet80_Eta2p1_v1=0;
    double counter_L1_SingleJet16_BptxAND=0;
+   double counter_L1_SingleJet24_BptxAND=0;
    double counter_L1_SingleJet28_BptxAND=0;
    double counter_L1_SingleJet40_BptxAND=0;
    double counter_L1_SingleJet48_BptxAND=0;
@@ -82,20 +85,21 @@ void plotMBRatesPP(int totall1rate=1600*1.4*11245){
    for (Long64_t i=0; i<nentries;i++) {
       hlt->GetEntry(i);
 
-      if(HLT_DmesonPPTrackingGlobal_Dpt8_v1)counter_HLT_DmesonPPTrackingGlobal_Dpt8_v1++;
-      if(HLT_DmesonPPTrackingGlobal_Dpt15_v1)counter_HLT_DmesonPPTrackingGlobal_Dpt15_v1++;
-      if(HLT_DmesonPPTrackingGlobal_Dpt20_v1)counter_HLT_DmesonPPTrackingGlobal_Dpt20_v1++;
-      if(HLT_DmesonPPTrackingGlobal_Dpt30_v1)counter_HLT_DmesonPPTrackingGlobal_Dpt30_v1++;
-      if(HLT_DmesonPPTrackingGlobal_Dpt40_v1)counter_HLT_DmesonPPTrackingGlobal_Dpt40_v1++;
-      if(HLT_DmesonPPTrackingGlobal_Dpt50_v1)counter_HLT_DmesonPPTrackingGlobal_Dpt50_v1++;
-      if(HLT_DmesonPPTrackingGlobal_Dpt60_v1)counter_HLT_DmesonPPTrackingGlobal_Dpt60_v1++;
-      if(HLT_AK4PFBJetBSSV80_Eta2p1_v1)counter_HLT_AK4PFBJetBSSV80_Eta2p1_v1++;
-      if(HLT_AK4PFBJetBSSV60_Eta2p1_v1)counter_HLT_AK4PFBJetBSSV60_Eta2p1_v1++;
-      if(HLT_AK4PFBJetBCSV80_Eta2p1_v1)counter_HLT_AK4PFBJetBCSV80_Eta2p1_v1++;
-      if(HLT_AK4PFBJetBCSV60_Eta2p1_v1)counter_HLT_AK4PFBJetBCSV60_Eta2p1_v1++;
+      if(HLT_DmesonPPTrackingGlobal_Dpt8_v1&&L1_SingleJet16_BptxAND)counter_HLT_DmesonPPTrackingGlobal_Dpt8_v1++;
+      if(HLT_DmesonPPTrackingGlobal_Dpt15_v1&&L1_SingleJet24_BptxAND)counter_HLT_DmesonPPTrackingGlobal_Dpt15_v1++;
+      if(HLT_DmesonPPTrackingGlobal_Dpt20_v1&&L1_SingleJet28_BptxAND)counter_HLT_DmesonPPTrackingGlobal_Dpt20_v1++;
+      if(HLT_DmesonPPTrackingGlobal_Dpt30_v1&&L1_SingleJet40_BptxAND)counter_HLT_DmesonPPTrackingGlobal_Dpt30_v1++;
+      if(HLT_DmesonPPTrackingGlobal_Dpt40_v1&&L1_SingleJet40_BptxAND)counter_HLT_DmesonPPTrackingGlobal_Dpt40_v1++;
+      if(HLT_DmesonPPTrackingGlobal_Dpt50_v1&&L1_SingleJet48_BptxAND)counter_HLT_DmesonPPTrackingGlobal_Dpt50_v1++;
+      if(HLT_DmesonPPTrackingGlobal_Dpt60_v1&&L1_SingleJet48_BptxAND)counter_HLT_DmesonPPTrackingGlobal_Dpt60_v1++;
+      if(HLT_AK4PFBJetBSSV80_Eta2p1_v1&&L1_SingleJet48_BptxAND)counter_HLT_AK4PFBJetBSSV80_Eta2p1_v1++;
+      if(HLT_AK4PFBJetBSSV60_Eta2p1_v1&&L1_SingleJet40_BptxAND)counter_HLT_AK4PFBJetBSSV60_Eta2p1_v1++;
+      if(HLT_AK4PFBJetBCSV80_Eta2p1_v1&&L1_SingleJet48_BptxAND)counter_HLT_AK4PFBJetBCSV80_Eta2p1_v1++;
+      if(HLT_AK4PFBJetBCSV60_Eta2p1_v1&&L1_SingleJet40_BptxAND)counter_HLT_AK4PFBJetBCSV60_Eta2p1_v1++;
       if(HLT_AK4PFDJet60_Eta2p1_v1)counter_HLT_AK4PFDJet60_Eta2p1_v1++;
       if(HLT_AK4PFDJet80_Eta2p1_v1)counter_HLT_AK4PFDJet80_Eta2p1_v1++;
       if(L1_SingleJet16_BptxAND)counter_L1_SingleJet16_BptxAND++;
+      if(L1_SingleJet24_BptxAND)counter_L1_SingleJet24_BptxAND++;
       if(L1_SingleJet28_BptxAND)counter_L1_SingleJet28_BptxAND++;
       if(L1_SingleJet40_BptxAND)counter_L1_SingleJet40_BptxAND++;
       if(L1_SingleJet48_BptxAND)counter_L1_SingleJet48_BptxAND++;
@@ -124,6 +128,7 @@ void plotMBRatesPP(int totall1rate=1600*1.4*11245){
    cout<<"HLT_AK4PFDJet80_Eta2p1_v1="<<counter_HLT_AK4PFDJet80_Eta2p1_v1/counter_L1_ZeroBias*totall1rate<<" Hz"<<endl;
    
    cout<<"L1_SingleJet16_BptxAND="<<counter_L1_SingleJet16_BptxAND/counter_L1_ZeroBias*totall1rate<<" Hz"<<endl;
+   cout<<"L1_SingleJet24_BptxAND="<<counter_L1_SingleJet24_BptxAND/counter_L1_ZeroBias*totall1rate<<" Hz"<<endl;
    cout<<"L1_SingleJet28_BptxAND="<<counter_L1_SingleJet28_BptxAND/counter_L1_ZeroBias*totall1rate<<" Hz"<<endl;
    cout<<"L1_SingleJet40_BptxAND="<<counter_L1_SingleJet40_BptxAND/counter_L1_ZeroBias*totall1rate<<" Hz"<<endl;
    cout<<"L1_SingleJet48_BptxAND="<<counter_L1_SingleJet48_BptxAND/counter_L1_ZeroBias*totall1rate<<" Hz"<<endl;
